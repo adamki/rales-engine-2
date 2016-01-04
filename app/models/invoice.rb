28283aef870bcc720824a97b673dddbd2f761243
adamki <adajensen@gmail.com>
2016-01-04 15:57:12 -0700

    set relations between DB tables

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,7 @@
 class Invoice < ActiveRecord::Base
+  belongs_to :customer
+  belongs_to :merhant
+
+  has_many :transactions
+  has_many :invoice_items
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,5 @@
 class Merchant < ActiveRecord::Base
+  has_many :items
+  has_many :invoices
+  has_many :transactions, through: :invoices
 end
