57abdd8b4cbf004904f04ad824c5df0cf5f49d1e
adamki <adajensen@gmail.com>
2016-01-05 12:34:17 -0700

    removed service model and pushed CSV importing to lib/tasks/import.rake

--- test/controllers/api/v1/customers_controller_test.rb
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -11,6 +11,20 @@ class Api::V1::CustomersControllerTest < ActionController::TestCase
     assert_equal customer_count, json_response.count
   end
 
+  test "#index contains customers that have the correct properties" do
+    get :index, format: :json
+
+    json_response = JSON.parse(response.body)
+
+    json_response.each do |customer|
+      assert customer["id"]
+      assert customer["first_name"]
+      assert customer["last_name"]
+      assert customer["created_at"]
+      assert customer["updated_at"]
+    end
+  end
+
   test "#show" do
     customer = customers(:one)
     get :show, id: customer.id, format: :json
@@ -20,6 +34,12 @@ class Api::V1::CustomersControllerTest < ActionController::TestCase
     assert_response :success 
     assert_equal "Jeff", json_response["first_name"]
     assert_equal "Casimir", json_response["last_name"]
+  end
+
+  test "#show returns only one record" do
+    get :show, id: customers(:one), format: :json
+    
+    assert_kind_of Hash, json_response
   end
 
   test "#find" do
@@ -39,5 +59,6 @@ class Api::V1::CustomersControllerTest < ActionController::TestCase
     assert_response :success
     assert_equal "Jorge", json_response["first_name"]
   end
+
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,19 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'minitest/pride'
 require 'rails/test_help'
+SimpleCov.start 'rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionController::TestCase
+
+  def json_response
+    JSON.parse(response.body)
+  end
+
 end
