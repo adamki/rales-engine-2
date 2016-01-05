040f1d5abd289b4c355dec20881abb419c9ab398
adamki <adajensen@gmail.com>
2016-01-04 18:11:42 -0700

    index and show for api/v1/items is now completed

---
+++ test/controllers/api/v1/items_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class Api::V1::ItemsControllerTest < ActionController::TestCase
+  test "#index" do
+    item_count = Item.count
+    get :index, format: :json
+
+    json_response = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal item_count, json_response.count
+  end
+
+  test "#show" do
+    item = items(:one)
+    get :show, id: item.id, format: :json
+
+    json_response = JSON.parse(response.body)
+
+    assert_response :success 
+    assert_equal "Item Qui Esse", json_response["name"]
+    assert_equal "Some Description", json_response["description"]
+    assert_equal "1", json_response["merchant_id"]
+  end
+end
