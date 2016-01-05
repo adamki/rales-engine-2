22569cad942f6948d6124a3a886af2173ef01c07
adamki <adajensen@gmail.com>
2016-01-04 17:38:48 -0700

    smal change to make namespaced routes more specific. see routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants
-      resources :customers
+      resources :merchants, only: [:index, :show]
+      resources :customers,  only: [:index, :show]
     end
   end
 
